a090139aaa5d095d88f43a2335df8a65e2e28b15
pauline_grace <pedepolonia@up.edu.ph>
2021-08-27 22:14:08 +0800

    add user columns in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def first_name
+  end
+
+  def last_name
+  end
+
+  def employee_id
+  end
+
+  def supervisor
+  end
+
+
 end
