2d18c13e905a09c1af74f5bc10330434200fffe9
pauline_grace <pedepolonia@up.edu.ph>
2021-08-30 20:46:33 +0800

    create Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_151209) do
-ActiveRecord::Schema.define(version: 2021_08_27_142714) do
+ActiveRecord::Schema.define(version: 2021_08_30_124318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "intent"
+    t.string "status"
+    t.date "due"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "RxC"
