4ff74833e7ddc8059394df8e9855656c99bb63fe
pauline_grace <pedepolonia@up.edu.ph>
2021-08-31 20:32:59 +0800

    add index and create controllers for phase model

--- app/controllers/phases_controller.rb
+++ app/controllers/phases_controller.rb
@@ -1,6 +1,48 @@
 class PhasesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :get_project
+
     def index
+        @phases = @project.phases
     end 
+
+    def new
+        @phase = @project.phases.build
+    end
+
+
+
+    def create
+        @phase = @project.phases.build(phase_params)
+
+        if @phase.save
+            redirect_to project_phases_path, notice: "Phase was successfully created." 
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def get_project
+        @project = current_user.projects.find(params[:project_id])
+        # redirect_to projects_path, notice: "Not allowed to do that" if @project.nil?
+    end
+
+    def is_current_user
+        @phase = @project.phases.find(params[:id])
+        # redirect_to projects_path, notice: "Not allowed to do that" if @phase.nil?
+    end
+
+    # def set_task
+    #     @task = @category.tasks.find(params[:id])
+    # end
+
+    def phase_params
+        params.require(:phase).permit(:step, :start_time, :end_time, :durationinmin, :remarks, :project_id, :user_id)
+    end
+
+
 
 
 end
