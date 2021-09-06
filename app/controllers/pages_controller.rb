6391bb8cbdc3a6b3e3b3a75b38f80e72cc1d91fe
pauline_grace <pedepolonia@up.edu.ph>
2021-09-06 19:56:02 +0800

    add controllers for admin view of projects and phases

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,27 @@
 class PagesController < ApplicationController
+
     def home
-        @allprojects = Project.all
-        @allphases = Phase.all
     end
+
+    def index
+        @allprojects = Project.all
+        @totalprojects = Project.count
+    end
+
+    # def query_project
+    #     Project.where(params:[:intent])
+    # end
+
+    # def query_phases
+
+    # end
+
+    # def query_ticket_level
+    # end
+
+    # def query_state
+    # end
+
+
+    
 end
---
+++ test/controllers/project_allphases_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProjectAllphasesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
