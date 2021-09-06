2114f38d3afe3bff2db49a774a7d7528fb127afd
pauline_grace <pedepolonia@up.edu.ph>
2021-09-07 00:10:49 +0800

    modify controllers and views for admin view of phases and projects, remove hardcodes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
     def home
+        @allprojects = Project.all
+        @intent = Project.where(intent: 'PMTA Enhancement')
+        @allphases = Phase.all
     end
 
     def index
@@ -8,13 +11,27 @@ class PagesController < ApplicationController
         @totalprojects = Project.count
     end
 
-    # def query_project
-    #     Project.where(params:[:intent])
-    # end
+    def query_project
+        @intent = Project.where(intent: params[:intent]).group(:classification).average(:hours_spent)
 
-    # def query_phases
+        if @intent
+            render :home
+        end  
+    end
 
-    # end
+    def query_phase
+        byebug
+        @projects = Project.where(intent: params[:intent])
+
+        @projects.each do |p|
+            @phase_list = Phase.where(project_id: p.id).group(:step).average(:durationinmin)
+        end
+
+
+        if @projects
+            render :home
+        end 
+    end
 
     # def query_ticket_level
     # end
@@ -22,6 +39,10 @@ class PagesController < ApplicationController
     # def query_state
     # end
 
+    # private 
+    # def project_params
+    #     params.require(:project).permit(:RxC, :classification, :project_type, :state, :workflow_date, :ticket_level, :hours_spent, :days_spent, :notes, :intent, :category_id, :user_id)
+    # end
 
     
 end
