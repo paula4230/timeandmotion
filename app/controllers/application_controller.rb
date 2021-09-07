a78fd5e2f607e46bd5ee2e24b9a7d93fb2b9e29e
pauline_grace <pedepolonia@up.edu.ph>
2021-09-07 21:49:37 +0800

    set user-specific timezones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller? 
+    around_action :set_time_zone, if: :current_user
+    # before_action :foo
+
+
+
+    private
+
+    def set_time_zone(&block)
+       Time.use_zone(current_user.time_zone, &block)
+    end
+
+    # def foo
+    #     byebug
+    # end
 
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:admin, :first_name, :last_name, :employee_id, :supervisor])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:admin, :first_name, :last_name, :employee_id, :supervisor, :time_zone])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:admin, :first_name, :last_name, :employee_id, :supervisor, :time_zone])
     end
 end
